dfc89065e5a792cbde32ee322eb72f89380b89b6
BenjaminKindelsberger <be.kindelsberger@gmail.com>
2020-11-12 19:17:21 +0100

    Frontend backend

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const app = express();
+
+const commit = require("./models/commit");
+const dbURL = "mongodb://127.0.0.1:27017/Commits";
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+app.use(express.static('public'));
+
+app.get("/showall", async (req, res) => {
+    commit.find().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        res.send(err);
+    })
+})
+
+app.get("/random",cors(), async (req, res) => {
+    commit.countDocuments().exec(function (err, count) {
+        var random = Math.floor(Math.random() * count)
+
+        commit.findOne().skip(random).exec(
+            function (err, result) {
+                res.send(result);
+            })
+    })
+})
