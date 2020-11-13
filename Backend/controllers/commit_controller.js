7fb0a510006dbb03468a59937a9eb7861bf11feb
BenjaminKindelsberger <be.kindelsberger@gmail.com>
2020-11-13 10:37:00 +0100

    Update votes possible and changed to cleaner code

--- Backend/controllers/commit_controller.js
+++ Backend/controllers/commit_controller.js
@@ -1,9 +1,31 @@
 const commit = require("../models/commit");
 
-exports.update = async function(req, res){
+exports.findAll = async function(req, res){
+    commit.find().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        res.send(err);
+    })
+}
+
+exports.findRandom = async function(req,res){
+
+        commit.countDocuments().exec(function (err, count) {
+            var random = Math.floor(Math.random() * count)
+    
+            commit.findOne().skip(random).exec(
+                function (err, result) {
+                    res.send(result);
+                })
+        })
+}
+
+exports.updateVotes = async function(req, res){
 
     const doc = await commit.findOne({_id: req.params.id})
     console.log(doc.votes)
     doc.votes++;
     await doc.save()
+    res.send(null)
+    
 }
--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const app = express();
-
 const controller = require("./controllers/commit_controller")
-const commit = require("./models/commit");
 const dbURL = "mongodb://127.0.0.1:27017/Commits";
+
+
 
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((res) => app.listen(3000))
@@ -13,26 +13,10 @@ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(express.static('public'));
 
-app.get("/showall", async (req, res) => {
-    commit.find().then((result) => {
-        res.send(result);
-    }).catch((err) => {
-        res.send(err);
-    })
-})
+app.get("/showall", cors(), controller.findAll);
+    
+app.get("/random",cors(), controller.findRandom)
 
-
-app.get("/random",cors(), async (req, res) => {
-    commit.countDocuments().exec(function (err, count) {
-        var random = Math.floor(Math.random() * count)
-
-        commit.findOne().skip(random).exec(
-            function (err, result) {
-                res.send(result);
-            })
-    })
-})
-
-app.put("/addvote/:id", cors(), controller.update);
+app.put("/addvote/:id", cors(), controller.updateVotes);
 
 
